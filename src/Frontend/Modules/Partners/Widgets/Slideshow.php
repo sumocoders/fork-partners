214c8541b1b0e2abe63eed9fc11262a41cffa735
Jelmer Prins <jelmer@sumocoders.be>
2017-01-17 14:06:41 +0100

    Update the widget

--- src/Frontend/Modules/Partners/Widgets/Slideshow.php
+++ src/Frontend/Modules/Partners/Widgets/Slideshow.php
@@ -2,38 +2,17 @@
 
 namespace Frontend\Modules\Partners\Widgets;
 
-/*
- * This file is part of Fork CMS.
- *
- * For the full copyright and license information, please view the license
- * file that was distributed with this source code.
- */
-
 use Frontend\Core\Engine\Base\Widget as FrontendBaseWidget;
-use Frontend\Modules\Partners\Engine\Model as FrontendPartnersModel;
 
-/**
- * This is a widget for the partner slideshow
- *
- * @author Jelmer Prins <jelmer@sumocoders.be>
- */
 class Slideshow extends FrontendBaseWidget
 {
-    /**
-     * Execute the extra
-     */
     public function execute()
     {
         parent::execute();
         $this->loadTemplate();
-        $this->parse();
-    }
-
-    /**
-     * Parse
-     */
-    private function parse()
-    {
-        $this->tpl->assign('partners', FrontendPartnersModel::getSlidersPartners($this->data['id']));
+        $this->tpl->assign(
+            'partners',
+            $this->get('partners.repository.widget')->find($this->data['id'])->getPartners()
+        );
     }
 }
