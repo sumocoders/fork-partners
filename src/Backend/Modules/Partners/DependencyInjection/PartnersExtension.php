007468520ffb04de89214c4c8439f4c803f4692f
Jelmer Prins <jelmer@sumocoders.be>
2017-01-17 10:40:21 +0100

    Fix DI config namespace

--- src/Backend/Modules/Partners/DependencyInjection/PartnersExtension.php
+++ src/Backend/Modules/Partners/DependencyInjection/PartnersExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Backend\Modules\Appeals\DependencyInjection;
+namespace Backend\Modules\Partners\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
