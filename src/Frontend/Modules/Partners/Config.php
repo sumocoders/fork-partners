d732b8da3baff336b416131f444ab3ee044f7a4d
Jelmer Prins <jelmer@sumocoders.be>
2017-01-17 14:03:17 +0100

    Frontend just has the default config

--- src/Frontend/Modules/Partners/Config.php
+++ src/Frontend/Modules/Partners/Config.php
@@ -1,33 +1,9 @@
 <?php
 
 namespace Frontend\Modules\Partners;
-/*
- * This file is part of Fork CMS.
- *
- * For the full copyright and license information, please view the license
- * file that was distributed with this source code.
- */
 
 use Frontend\Core\Engine\Base\Config as FrontendBaseConfig;
 
-/**
- * This is the configuration-object
- *
- * @author Jelmer Prins <jelmer@sumocoders.be>
- */
 final class Config extends FrontendBaseConfig
 {
-    /**
-     * The default action
-     *
-     * @var    string
-     */
-    protected $defaultAction = 'index';
-
-    /**
-     * The disabled actions
-     *
-     * @var    array
-     */
-    protected $disabledActions = array();
 }
