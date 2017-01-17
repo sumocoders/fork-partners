e2858fd9f90d2811dc7e5651402d56980293b1f9
Jelmer Prins <jelmer@sumocoders.be>
2017-01-17 10:51:45 +0100

    Data transfer object for the partner

---
+++ src/Backend/Modules/Partners/Domain/Partner/PartnerDataTransferObject.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Backend\Modules\Partners\Domain\Partner;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PartnerDataTransferObject
+{
+    /**
+     * @var Partner|null
+     */
+    private $partnerEntity;
+
+    /**
+     * @var Image
+     */
+    public $image;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(message="err.FieldIsRequired")
+     */
+    public $name;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(message="err.FieldIsRequired")
+     * @Assert\Url(message="err.InvalidURL")
+     */
+    public $url;
+
+    /**
+     * @param Partner|null $partner
+     */
+    public function __construct(Partner $partner = null)
+    {
+        $this->partnerEntity = $partner;
+
+        if (!$this->hasExistingPartner()) {
+            return;
+        }
+
+        $this->image = $partner->getImage();
+        $this->name = $partner->getName();
+        $this->url = $partner->getUrl();
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasExistingPartner()
+    {
+        return $this->partnerEntity instanceof Partner;
+    }
+
+    /**
+     * @return Partner
+     */
+    public function getPartnerEntity()
+    {
+        return $this->partnerEntity;
+    }
+}
